1b8dad958f8c73faa17bb404aca6f405bb200de5
Sue Nolte <sznolte@gmail.com>
2024-01-24 15:30:44 -0500

    remove proxy map in the nginx.conf

--- compose/shiny/shiny-server.sh
+++ compose/shiny/shiny-server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
+echo "Hello I am shiny-server shell file.  $USER"
+#mkdir -p /var/log/shiny-server
+#chown shiny.shiny /var/log/shiny-server
 
-exec shiny-server 2>&1
+#exec shiny-server 2>&1
